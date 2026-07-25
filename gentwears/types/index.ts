459421e79e8d6f6d5afad2ce54528aca267f598e
Toriola Anjolaoluwa <mujibatanjola@gmail.com>
2026-07-25 01:27:54 +0100

    Update index.ts

--- gentwears/types/index.ts
+++ gentwears/types/index.ts
@@ -1,10 +1,16 @@
+export type RetailBrand = "Gentwears" | "Nike" | "Adidas" | "New Balance" | "Rick Owens" | "ASICS";
+
 export interface Product {
   id: number;
   slug: string;
   name: string;
+  brand?: string;
+  model?: string;
+  retailBrand: RetailBrand;
   price: number;
   category: "Clothing" | "Shoes";
   collection: "Signature" | "New Arrivals";
+  shoeCollection?: "Signature Collection" | "Luxury Sneakers" | "Streetwear Sneakers" | "Everyday Sneakers" | "Boots";
   images: [string, string];
   sizes: string[];
   description: string;
