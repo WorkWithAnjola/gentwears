da071fd1d2c1a9563fc6403c396a54cd1bbd4b5a
Toriola Anjolaoluwa <mujibatanjola@gmail.com>
2026-09-04 21:33:01 +0100

    Update page.tsx

--- gentwears/app/page.tsx
+++ gentwears/app/page.tsx
@@ -1,17 +1,7 @@
-import { PRODUCTS } from "@/data/products";
-import Hero from "@/components/home/Hero";
-import BrandStatement from "@/components/home/BrandStatement";
-import CategoryGrid from "@/components/home/CategoryGrid";
-import ShopByBrandGrid from "@/components/home/ShopByBrandGrid";
-import ProductRow from "@/components/home/ProductRow";
-import CTABand from "@/components/home/CTABand";
-import Reviews from "@/components/home/Reviews";
-import Newsletter from "@/components/home/Newsletter";
-
 export default function HomePage() {
   const signature = PRODUCTS.filter((p) => p.tag === "House Brand");
-const newArrivals = PRODUCTS.filter((p) => p.category === "Sneakers");
-const newArrivals = PRODUCTS.filter((p) => p.category === "Sneakers");
+  const newArrivals = PRODUCTS.filter((p) => p.category === "Sneakers");
+  const bestSellers = PRODUCTS.filter((p) => p.tag === "House Brand");
 
   return (
     <>
