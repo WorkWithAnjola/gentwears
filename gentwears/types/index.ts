5787d95d6f2f5da2aaacd64080ade52aa44219d3
Toriola Anjolaoluwa <mujibatanjola@gmail.com>
2026-09-05 12:15:28 +0100

    Refactor Product interface for flexibility

--- gentwears/types/index.ts
+++ gentwears/types/index.ts
@@ -1,20 +1,20 @@
 export type RetailBrand = "Gentwears" | "Nike" | "Adidas" | "New Balance" | "Rick Owens" | "ASICS";
 
 export interface Product {
-  id: number;
+  id: string | number;
   slug: string;
   name: string;
   brand?: string;
   model?: string;
-  retailBrand: RetailBrand;
+  retailBrand?: RetailBrand | string;
   price: number;
-  category: "Clothing" | "Shoes";
-  collection: "Signature" | "New Arrivals";
-  shoeCollection?: "Signature Collection" | "Luxury Sneakers" | "Streetwear Sneakers" | "Everyday Sneakers" | "Boots";
-  images: [string, string];
-  sizes: string[];
-  description: string;
-  tag?: "New" | "Best Seller" | "";
+  category: string;
+  collection?: string;
+  shoeCollection?: string;
+  images: string[] | [string, string];
+  sizes?: string[];
+  description?: string;
+  tag?: string;
 }
 
 export interface CartItem extends Product {
